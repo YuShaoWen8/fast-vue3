dd5226f64bab56940e5eb0f14907319e8b2ee11d
yushaowen <yu.shaowen@code-dance.com>
2022-08-04 13:47:35 +0800

    初始化项目

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import routes from 'virtual:generated-pages';
+// import routes from 'virtual:generated-pages';
+import routes from './root';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 
-routes.push({
-  path: '/',
-  redirect: '/login',
-});
+// routes.push({
+//   path: '/',
+//   redirect: '/demo',
+// });
+// console.log(routes);
+// debugger;
 //导入生成的路由数据
+console.log(routes, '路由信息');
+debugger;
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
@@ -18,7 +23,8 @@ router.beforeEach(async (_to, _from, next) => {
   next();
 });
 
-router.afterEach((_to) => {
+//配置路由元信心
+router.afterEach((_to, _from) => {
   NProgress.done();
 });
 
--- src/router/root.ts
+++ src/router/root.ts
@@ -1,10 +1,10 @@
 export default [
   {
     path: '/',
-    component: () => import('@/pages/login/index.vue'),
+    component: () => import('/@/pages/login/index.vue'),
   },
   {
-    path: 'shi',
-    component: () => import('@/pages/shi/index.vue'),
+    path: '/shi',
+    component: () => import('/@/pages/shi/index.vue'),
   },
 ];
